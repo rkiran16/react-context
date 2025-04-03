6dcc5e43f4593c45d3afe50d3374cde986782d4d
Ravi Kanculakunta (C) <ravikancula@gmail.com>
2025-04-03 11:38:21 -0700

    added tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ const CountButtons = () => {
    const { dispatch } = useAppContext();
    return (
       <div>
-         <button className='bg-white' onClick={() => dispatch({type: "increment"})}>Increment Count</button>
-         <button className='bg-white' onClick={() => dispatch({type: "decrement"})}>Decrement Count</button>
+         <button className='rounded bg-violet-500 hover:bg-violet-700 focus:outline-0 focus:outline-offset-0 px-4 py-2 text-base text-white mx-1' onClick={() => dispatch({type: "increment"})}>Increment Count</button>
+         <button className='rounded bg-violet-500 hover:bg-violet-700 px-4 py-2 text-base text-white' onClick={() => dispatch({type: "decrement"})}>Decrement Count</button>
       </div>
    )
 }
@@ -19,8 +19,10 @@ const CountButtons = () => {
 function App() {
   return (
     <AppProvider>
-        <CountDisplay />
-        <CountButtons />
+        <div className='mx-auto max-w-7xl flex justify-center'>
+          <CountDisplay />
+          <CountButtons />
+        </div>
     </AppProvider>
   )
 }
